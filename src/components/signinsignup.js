ff7b5953a66b9bf6d87fe237f542b3293c71fe72
James Lagattuta <jimmy.lagattuta@gmail.com>
2017-10-06 02:52:38 -0700

    user creation completed

--- src/components/signinsignup.js
+++ src/components/signinsignup.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 // import { signUp, signIn } from '../actions';
 import { Field, reduxForm } from 'redux-form';
 import './signinsignup.css'; 
+import axios from 'axios';
 
 class SignInSignUp extends Component {
 	constructor(props) {
@@ -15,6 +16,10 @@ class SignInSignUp extends Component {
 
 	componentWillMount() {
 		console.log('componentWillMount');
+   		const userId = sessionStorage.getItem('userId');
+   		console.log('userID below');
+   		console.log(userId);
+
 	}
 
 	onClickSignChange() {
@@ -25,15 +30,98 @@ class SignInSignUp extends Component {
 			this.setState({ signUp: false });
 		}
 	}
+    renderTextField(field) {
+	    const { meta: {touched, error} } = field
+	    const className = `form-group ${ touched && error ? 'has-danger' : '' }`;
+
+	    return (
+	      <div className={className} id="field-form">
+	        <label>{field.label}</label>
+	        <input
+	          className="form-control"
+	          type="text"
+	          {...field.input}
+	          placeholder={field.label}
+	        />
+	        <div className="text-help">
+	          {touched ? error : ''}
+	        </div>
+	      </div>
+	    );
+    }
+    renderPasswordField(field) {
+	    const { meta: {touched, error} } = field
+	    const className = `form-group ${ touched && error ? 'has-danger' : '' }`;
+
+	    return (
+	      <div className={className} id="field-form">
+	        <label>{field.label}</label>
+	        <input
+	          className="form-control"
+	          type="password"
+	          {...field.input}
+	          placeholder={field.label}
+	        />
+	        <div className="text-help">
+	          {touched ? error : ''}
+	        </div>
+	      </div>
+	    );
+    }
+    onSubmit(values) {
+    	console.log('onSubmit');
+    	console.log('values below');
+    	console.log(values);
+   		// const userId = sessionStorage.getItem('userId');
+
+    	axios.post('http://localhost:3000/api/v1/sessions/signin', values)
+    		.then(payload => {
+    			console.log('payload below');
+    			console.log(payload.data);
+        		alert('user signed in');
+        		window.location = "http://localhost:3001";
+       		})
+       		.catch(err => {alert(err)});
+    }
+    onSubmitSignup(values) {
+    	console.log('onSubmitSignup');
+    	console.log('values below');
+    	console.log(values);
+   		// const userId = sessionStorage.getItem('userId');
+
+   		axios.post('http://localhost:3000/api/v1/users', values)
+   			.then(payload => {
+   				console.log('payload below');
+   				console.log(payload.data);
+   				alert('user created, you can now sign up using you username');
+   				window.location = "http://localhost:3001";
+   			})
+   			.catch(err => {alert(err)});
+    }
 
 	render() {
+	    const { handleSubmit } = this.props;
+
 		if(this.state.signUp === false) {
 			return (
 				<div className="signinsignup_top_left">
 					<div className="signinsignup_top_left_content">
-					<div onClick={this.onClickSignChange}>
-						<h3>Sign In!</h3>
-					</div>
+						<div onClick={this.onClickSignChange}>
+							<h3>Sign In!(click me)</h3>
+						</div>
+						<form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+							<Field
+								label="Username "
+								name="username_signin"
+								component={this.renderTextField.bind(this)}
+							/>
+							<Field
+								label="Password "
+								name="password_signin"
+								component={this.renderPasswordField.bind(this)}
+							/>
+	                        <button className="btn btn-success" type="submit">Submit</button>
+						</form>
 					</div>
 				</div>
 			);
@@ -42,8 +130,32 @@ class SignInSignUp extends Component {
 				<div className="signinsignup_top_left">
 					<div className="signinsignup_top_left_content">
 						<div onClick={this.onClickSignChange}>
-							<h3>Sign Out!</h3>
+							<h3>Sign Up!(click me)</h3>
 						</div>
+						<form onSubmit={handleSubmit(this.onSubmitSignup.bind(this))}>
+							<Field
+								label="Username "
+								name="username"
+								component={this.renderTextField.bind(this)}
+							/>
+							<Field 
+								label="Password "
+								name="password"
+								component={this.renderPasswordField.bind(this)}
+							/>
+							<Field
+								label="Password Confirmation "
+								name="password_confirmation"
+								component={this.renderPasswordField.bind(this)}
+							/>
+							<Field
+								label="Email "
+								name="email"
+								component={this.renderTextField.bind(this)}
+							/>
+							<button className="btn btn-success" type="submit">Submit</button>
+
+						</form>
 					</div>
 				</div>
 			);
@@ -51,4 +163,27 @@ class SignInSignUp extends Component {
 	};
 };
 
-export default SignInSignUp;
+function validate(values) {
+  const errors = {};
+  if (!values.username) {
+    errors.username = 'Enter a Username';
+  }
+  if (!values.password) {
+    errors.password = "Enter a Password";
+  }
+  if (values.password_confirmation !== values.password) {
+  	errors.password_confirmation = "Your Passwords Dont Match";
+  }
+  if (!values.email) {
+  	errors.email = "Enter your Email"
+  }
+  return errors;
+}
+
+export default reduxForm({
+	validate,
+	form: 'signUp',
+	form: 'signIn'
+})(
+ connect(null)(SignInSignUp)
+);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import StocksReducer from './reducer_stocks';
 import SearchStockReducer from './reducer_search_stock';
 
 const rootReducer = combineReducers({
+	form: formReducer,
 	stocks: StocksReducer,
 	searchStock: SearchStockReducer
 });
