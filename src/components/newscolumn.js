6e4c0923c91f09796c0024f7bda7852376b718f1
James Lagattuta <jimmy.lagattuta@gmail.com>
2017-10-28 00:24:31 -0700

    conditional rendering for news column array, limits news stories to ten per date

--- src/components/newscolumn.js
+++ src/components/newscolumn.js
@@ -190,31 +190,154 @@ class NewsColumn extends Component {
 function renderNews(array) {
 	console.log('array renderNews here data');
 	console.log(array);
+	let date_count = 0;
+	let date_array = [];
+	let new_date_boolean = false;
 	return _.map(array, item => {
 		const date = item.publication_date.slice(0, 10);
-		console.log('date', date);
+		// console.log('date', date);
 		const cut_date = item.publication_date.slice(0, 8);
-		console.log('cut_date', cut_date);
+		// console.log('cut_date', cut_date);
 		const temp_date = item.publication_date.slice(8, 10);
-		console.log('temp_date', temp_date);
+		// console.log('temp_date', temp_date);
 		const temp_date_two = temp_date - 1;
-		console.log('temp_date_two', temp_date_two);
+		// console.log('temp_date_two', temp_date_two);
 		const accurate_date = cut_date.toString() + temp_date_two;
-		console.log('accurate_date', accurate_date);
-		console.log('forEach item here', item);
-		return (
-			<div key={item.id}>
-				<div id="news">
-					<p id="publication_date">Published: {accurate_date}
-						<form id="link" target="_blank" action={item.url}>
-    						<input type="submit" value="News Story Link" />
-						</form>
-					</p>
-					<p id="newsrendertitle">{item.title}</p>
-					<p>{item.summary}</p>
-				</div>
-			</div>
-		);
+		// console.log('accurate_date', accurate_date);
+		// console.log('forEach item here', item)
+		if(date_array.includes(accurate_date)) {
+			console.log('1');
+			new_date_boolean = false;
+		} else if(date_array.includes(accurate_date) === false) {
+			console.log('3');
+			new_date_boolean = true;
+			// date_array.push(accurate_date);
+			date_array.push(accurate_date);
+			date_count = 0;
+		} 
+		// else if(date_array.includes(accurate_date) && date_count ) {
+		// 	console.log('2');
+		// 	new_date_boolean = false;
+		// }
+
+
+		if(new_date_boolean === true && date_count === 0) {
+			console.log('4');
+			console.log('date_count old ', date_count);
+			date_count += 1;
+			console.log('date_count new ', date_count);
+			console.log('date_array old ', date_array);
+			console.log('date_array new ', date_array);
+			return (
+					<div key={item.id}>
+						<div id="news">
+							<p id="publication_date">Published: {accurate_date}
+								<form id="link" target="_blank" action={item.url}>
+			    					<input type="submit" value="News Story Link" />
+								</form>
+							</p>
+							<p id="newsrendertitle">{item.title}</p>
+							<p>{item.summary}</p>
+						</div>
+					</div>
+				);	
+		} else if(new_date_boolean === false && date_count < 10) {
+			console.log('5');
+			console.log('date_count old ', date_count);
+			date_count += 1;
+			console.log('date_count new ', date_count);
+			return (
+					<div key={item.id}>
+						<div id="news">
+							<p id="publication_date">Published: {accurate_date}
+								<form id="link" target="_blank" action={item.url}>
+			    					<input type="submit" value="News Story Link" />
+								</form>
+							</p>
+							<p id="newsrendertitle">{item.title}</p>
+							<p>{item.summary}</p>
+						</div>
+					</div>
+				);	
+		}
+
+
+
+
+
+
+
+
+		// if(date_array.includes(accurate_date) && date_count === 10) {
+		// 	console.log('5');
+		// 	date_count = 0;
+		// 	console.log('date_array, ', date_array);
+		// 	return (
+		// 			<div key={item.id}>
+		// 				<div id="news">
+		// 					<p id="publication_date">Published: {accurate_date}
+		// 						<form id="link" target="_blank" action={item.url}>
+		// 	    					<input type="submit" value="News Story Link" />
+		// 						</form>
+		// 					</p>
+		// 					<p id="newsrendertitle">{item.title}</p>
+		// 					<p>{item.summary}</p>
+		// 				</div>
+		// 			</div>
+		// 		);
+		// } else if(date_array.includes(accurate_date) === false && new_date_boolean === false) {
+		// 		// new_date_boolean = false;
+		// 		console.log('1');
+		// 		console.log('date_count before', date_count);
+		// 		date_count += 1;
+		// 		console.log('date_count after', date_count);
+		// 		if(date_count !== 10) {
+		// 			console.log('2');
+		// 			return (
+		// 				<div key={item.id}>
+		// 					<div id="news">
+		// 						<p id="publication_date">Published: {accurate_date}
+		// 							<form id="link" target="_blank" action={item.url}>
+		// 	    						<input type="submit" value="News Story Link" />
+		// 							</form>
+		// 						</p>
+		// 						<p id="newsrendertitle">{item.title}</p>
+		// 						<p>{item.summary}</p>
+		// 					</div>
+		// 				</div>
+		// 			);
+		// 		} else {
+		// 			console.log('3');
+
+
+		// 		}
+		// 	}if (date_count !== 10 && new_date_boolean === true && date_count !== 0) {
+		// 		console.log('new_date_boolean', new_date_boolean);
+		// 		console.log('6');			
+		// 	} else {
+		// 		// new_date_boolean = true;
+		// 		console.log('4');
+		// 		console.log('date_count before', date_count);
+		// 		date_count += 1;
+		// 		date_array.push(accurate_date);
+		// 		console.log('date_count after', date_count);
+		// 		return (
+		// 			<div key={item.id}>
+		// 				<div id="news">
+		// 					<p id="publication_date">Published: {accurate_date}
+		// 						<form id="link" target="_blank" action={item.url}>
+		//     						<input type="submit" value="News Story Link" />
+		// 						</form>
+		// 					</p>
+		// 					<p id="newsrendertitle">{item.title}</p>
+		// 					<p>{item.summary}</p>
+		// 				</div>
+		// 			</div>
+		// 		);			
+		// 	} 
+			
+		// }
+
 	});
 }
 
